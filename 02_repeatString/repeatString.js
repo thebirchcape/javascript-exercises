a43972b94df098d51a26690b00badb67402f7d35
thebirchcape <koivueeroniemi@gmail.com>
2024-11-19 22:27:14 +0200

    ex2 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, num) {
+    result = ""
+    if (num<0) {
+        return "ERROR"
+    } else {
+        for (let i=0; i<num; i++) {
+            result = result+stringToRepeat
+        }
+        return result
+    }
 };
 
 // Do not edit below this line
