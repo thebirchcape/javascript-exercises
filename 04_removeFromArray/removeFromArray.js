93a149e8f79d4f73877f1787e9db607914695020
thebirchcape <koivueeroniemi@gmail.com>
2024-11-19 23:38:00 +0200

    ex4 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToModify) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    for (const arg of args) {
+        while (arrayToModify.includes(arg)) {
+            console.log(arg)
+            const index = arrayToModify.indexOf(arg);
+            if (index>=0) arrayToModify.splice(index, 1);
+        }
+        } 
+    return arrayToModify
 };
 
 // Do not edit below this line
