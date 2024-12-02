8befa214457d7bb18603e3da1583eaae56e79677
thebirchcape <koivueeroniemi@gmail.com>
2024-12-02 06:58:16 +0200

    ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let smaller = 0;
+    let larger = 0;
+    let summed = 0;
+    if (num1 < 0 || 
+        num2 < 0 || 
+        !Number.isInteger(num1) || 
+        !Number.isInteger(num2)
+    ) {
+        console.log("test")
+        return "ERROR"
+    };
+    if (num1 < num2) {
+        summed = num1;
+        smaller = num1 + 1;
+        larger = num2;
+    } else {
+        summed = num2;
+        smaller = num2 + 1;
+        larger = num1;
+    }
+    while (smaller <= larger) {
+        summed = summed + smaller;
+        smaller++;
+    }
+    console.log(summed)
+    return summed;
 };
-
+sumAll("asd", 4)
 // Do not edit below this line
 module.exports = sumAll;
