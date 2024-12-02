0cd3cfcd0493780c2d55d00106b4d730ef4f66c5
thebirchcape <koivueeroniemi@gmail.com>
2024-12-02 08:09:27 +0200

    ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,48 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summed = 0
+	for (const arg of array) {
+    summed += arg
+  }
+  return summed
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1
+  let i = 0
+  while (i < array.length) {
+    result *= array[i]
+    i++;
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let i = x;
+  let result = 1;
+	if (x === 0) {
+    return 1;
+  }
+  while (i > 1) {
+    result *= x
+    x--;
+    i--;
+  }
+  return result;
 };
 
+factorial(1)
 // Do not edit below this line
 module.exports = {
   add,
