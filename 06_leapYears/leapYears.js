633d6c42d58fc2562c816b23aec6d6d48d6df7ca
thebirchcape <koivueeroniemi@gmail.com>
2024-12-02 07:18:27 +0200

    ex6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true
+            }
+            return false
+        }
+        return true
+    } else {
+        return false
+    }
 };
 
+console.log(leapYears(1997))
 // Do not edit below this line
 module.exports = leapYears;
